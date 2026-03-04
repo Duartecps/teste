0fe52c45176467845112c7eea28ca08b0c930897
Duartecps <00001099962560sp@al.educacao.sp.gov.br>
2026-03-04 16:17:37 -0300

    versão 3.0

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 h=int(input("Digite um número  "))
 l=int(input("Digite outro número  "))
+m=int(input("Digite outro número"))
 
-total = h+l
-total2 = h-l
-total3 = h*l
-total4 = h/l
+total = h+l+m
+total2 = h-l+m
+total3 = h*l*m
+total4 = h/l/m
 
 pergunta=str(input("Qual operação você deseja realizar? Digite: soma, subtração, multiplicação ou divisão  "))
 if pergunta == "soma":
